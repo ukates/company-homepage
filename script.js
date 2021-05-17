c13b9cdb0780e4a95186763853c7ba165b6c40b3
ukates <derrickcates@icloud.com>
2021-05-17 18:55:19 -0400

    adding functionality to hamburger button

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const hamburger = document.querySelector('.hamburger'); 
+/*const hamburger = document.querySelector('.hamburger'); 
 const navLeft = document.querySelector('.nav-left');
 const navRight = document.querySelector('.nav-right');  
 
 hamburger.addEventListener('click', () => {
     navLeft.classList.toggle('show');
     navRight.classList.toggle('show');  
-});
+});*/
+
+const menuBtn = document.querySelector('.hamburger'); 
+const closeBtn = document.querySelector('.closebtn');
+const slideMenu = document.querySelector('.slidemenu'); 
+
+menuBtn.addEventListener('click', () => {
+    slideMenu.classList.toggle('open');
+}); 
+
+closeBtn.addEventListener('click', () => {
+    slideMenu.classList.toggle('open'); 
+}); 
